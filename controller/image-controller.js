c693c6cadfe74d7742dcafd5b07379015ead37bf
sahil <sahil27raj@gmail.com>
2024-02-15 00:21:31 +0530

    add the changes after solving the error

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,23 +1,26 @@
  import File from "../models/file.js";
  
- const uploadImage =async(req,res)=>{
-
-    console.log("req",req.file)
-   
-    const fileObj = {
-        path:req.file.path,
-        name:req.file.originalname
+ const uploadImage = async (req, res) => {
+    console.log("req", req.file);
+    
+    if (!req.file || !req.file.path) {
+        return res.status(400).json({ error: "File path is missing." });
     }
-try{
-  const file =  await File.create(fileObj);
-  return  res.status(200).json({path: `http://localhost:3001/file/${file._id}`});
+    
+    const fileObj = {
+        path: req.file.path,
+        name: req.file.originalname
+    };
 
-   }catch(error){
-    console.log("error",error)
-    res.status(500).json({error:error.message})
-   }
+    try {
+        const file = await File.create(fileObj);
+        return res.status(200).json({ path: `http://localhost:3001/file/${file._id}` });
+    } catch (error) {
+        console.log("error", error);
+        return res.status(500).json({ error: error.message });
+    }
+};
 
-}
 
 const downloadImage = async (req, res) => {
    try {
@@ -29,13 +32,11 @@ const downloadImage = async (req, res) => {
        file.downloadContent++;
        await file.save();
 
-       // Send the file for download
        res.download(file.path, file.name, (error) => {
            if (error) {
                console.error(error);
                return res.status(500).json({ error: "Error occurred while downloading the file" });
            }
-           // Add script to close the window after download
            res.end('<script>window.close();</script>');
        });
    } catch (error) {
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import { uploadImage, downloadImage } from '../controller/image-controller.js';
 import multer from 'multer';
-
+import upload from '../utils/upload.js';
 const router = express.Router();
 
 
-const fileUpload = multer({ 
-    limits: { fileSize: 5 * 1024 * 1024 } 
-});
 
-router.post('/upload', fileUpload.single('file'), uploadImage);
+router.post('/upload', upload.single('file'), uploadImage);
 
 router.get('/file/:id', downloadImage);
 
